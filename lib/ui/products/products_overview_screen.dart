0592808cf0531303f9f718135548269c2333ccf4
luong <luongck21v7k019@vlvh.ctu.edu.vn>
2022-12-13 18:54:20 +0700

    Them thong tin so san pham trong gio hang

--- lib/ui/products/products_overview_screen.dart
+++ lib/ui/products/products_overview_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:myshop/ui/cart/cart_screen.dart';
 import '../shared/app_drawer.dart';
+import '../cart/cart_manager.dart';
+import 'top_right_badge.dart';
 
 
 import 'products_grid.dart';
@@ -33,15 +35,27 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen>{
   }
 
   Widget buildShoppingCartIcon(){
-    return IconButton(
-      icon: const Icon(
-        Icons.shopping_cart,
+    return TopRightBadge(
+      data: CartManager().productCount,
+      child: IconButton(
+        icon: const Icon(
+          Icons.shopping_cart,
+        ),
+        onPressed: () {
+          Navigator.of(context).pushNamed(CartScreen.routeName);
+        },
       ),
-      onPressed: (){
-        Navigator.of(context).pushNamed(CartScreen.routeName);
-        print('Go to cart screen');
-      },
     );
+    
+    // return IconButton(
+    //   icon: const Icon(
+    //     Icons.shopping_cart,
+    //   ),
+    //   onPressed: (){
+    //     Navigator.of(context).pushNamed(CartScreen.routeName);
+    //     print('Go to cart screen');
+    //   },
+    // );
   }
 
   Widget buildProductFilterMenu() {
