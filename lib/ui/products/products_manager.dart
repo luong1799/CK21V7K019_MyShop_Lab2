73217488e4a234fe76b4cf3393ebe4e438e2839a
luong <luongck21v7k019@vlvh.ctu.edu.vn>
2022-12-13 20:34:26 +0700

    Quan ly trang thai voi provider - ProductsManager

--- lib/ui/products/products_manager.dart
+++ lib/ui/products/products_manager.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/foundation.dart';
+
 import '../../models/product.dart';
 
-class ProductsManager{
+class ProductsManager with ChangeNotifier {
   final List<Product> _items = [
     Product(
       id: 'p1',
@@ -38,11 +40,40 @@ class ProductsManager{
     ),
   ];
 
-  int get itemCount{
+  void addProduct(Product product) {
+    _items.add(
+      product.copyWith(
+        id: 'p${DateTime.now().toIso8601String()}',
+      ),
+    );
+    notifyListeners();
+  }
+
+  void updateProduct(Product product) {
+    final index = _items.indexWhere((item) => item.id == product.id);
+    if (index >= 0) {
+      _items[index] = product;
+      notifyListeners();
+    }
+  }
+
+  void toggleFavoriteStatus(Product product) {
+    final savedStatus = product.isFavorite;
+    product.isFavorite = !savedStatus;
+  }
+
+  void deleteProduct(String id) {
+    final index = _items.indexWhere((item) => item.id == id);
+    _items.removeAt(index);
+    notifyListeners();
+  }
+
+
+  int get itemCount {
     return _items.length;
   }
 
-  List <Product> get item {
+  List<Product> get items {
     return [..._items];
   }
 
@@ -50,13 +81,11 @@ class ProductsManager{
     return _items.where((item) => item.isFavorite).toList();
   }
 
-  get items => null;
-
   Product? findById(String id) {
     try {
-        return _items.firstWhere((item) => item.id == id);
+      return _items.firstWhere((item) => item.id == id);
     } catch (error) {
-        return null;
+      return null;
     }
   }
 }
